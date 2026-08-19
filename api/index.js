fb5ad9e3417c75bd0e21b8ce8923060d6dc6cf4d
Marcos <marcosvinniio@gmail.com>
2026-08-19 13:56:05 -0300

    fix: reverte quebra proposital do teste

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ module.exports = function handler(request, response) {
 
   if (request.url === "/") {
     response.setHeader("Content-Type", "text/plain; charset=utf-8");
-    response.status(200).send("Olá, Mundo!");
+    response.status(200).send("Hello, World!");
   } else if (request.url === "/health") {
     response.setHeader("Content-Type", "application/json");
     response.status(200).send(
